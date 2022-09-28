9f523c2b02f4236215ef4058f81ed91b4fddfef9
tauhid-hasan-dev <tauhidhasan11@gmail.com>
2022-09-28 18:20:08 +0300

    react fontawesome added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faRectangleList } from '@fortawesome/free-solid-svg-icons'
 
 function App() {
   return (
     <div className="App">
       <h1>This is my first commit</h1>
+      <FontAwesomeIcon icon="fa-solid fa-rectangle-list" />
+      <FontAwesomeIcon icon={faRectangleList}></FontAwesomeIcon>
     </div>
   );
 }
